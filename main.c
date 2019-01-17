60b2616797776193a5c06d0855a375e617b27489
第一次 <123456456@qq.com>
2019-01-17 16:57:21 +0800

    基本框架

--- main.c
+++ main.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    printf("欢迎进入车位管理系统\n");
+
+    while (1)
+    {
+        printf("1---添加车位\n");
+        printf("2---删除车位\n");
+        printf("3---显示车位\n");
+        printf("4---最近的车位\n");
+        printf("5---退出\n");
+
+        printf("请输入相应的功能编号\n");
+
+        int code;
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+            printf("程序结束\n");
+            break;
+        }
+    }
 
     return 0;
-
 }
